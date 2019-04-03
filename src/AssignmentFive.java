34be535cabb0037e239cbe954658039fb6527828
Tyypos <tyyposofficial@gmail.com>
2019-04-03 06:26:31 -0700

    jared: initial set up

---
+++ src/AssignmentFive.java
@@ -0,0 +1,15 @@
+/**
+ * This application...
+ * 
+ * @author Team 6: Jared Cheney, Andrew Meraz, Chul Kim and Agustin Garcia
+ *
+ */
+public class AssignmentFive
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Welcome to assignment 5!");
+   }
+
+}
