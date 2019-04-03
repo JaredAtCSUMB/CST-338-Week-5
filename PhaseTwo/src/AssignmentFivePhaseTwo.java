c571420b73421c8e24b578ee35340f2d5d376a4d
Tyypos <tyyposofficial@gmail.com>
2019-04-03 07:14:12 -0700

    jared: more set up

--- PhaseTwo/src/AssignmentFivePhaseTwo.java
+++ PhaseTwo/src/AssignmentFivePhaseTwo.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
 /**
  * This application...
  * 
@@ -6,10 +9,54 @@
  */
 public class AssignmentFivePhaseTwo
 {
-
+   static int NUM_CARDS_PER_HAND = 7;
+   static int  NUM_PLAYERS = 2;
+   static JLabel[] computerLabels = new JLabel[NUM_CARDS_PER_HAND];
+   static JLabel[] humanLabels = new JLabel[NUM_CARDS_PER_HAND];  
+   static JLabel[] playedCardLabels  = new JLabel[NUM_PLAYERS]; 
+   static JLabel[] playLabelText  = new JLabel[NUM_PLAYERS]; 
+   
    public static void main(String[] args)
    {
-      System.out.println("Assignment Five Phase 2!");
-   }
+      int k;
+      Icon tempIcon;
+      
+      // establish main frame in which program will run
+      CardTable myCardTable 
+         = new CardTable("CardTable", NUM_CARDS_PER_HAND, NUM_PLAYERS);
+      myCardTable.setSize(800, 600);
+      myCardTable.setLocationRelativeTo(null);
+      myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+      // show everything to the user
+      myCardTable.setVisible(true);
+
+      // CREATE LABELS ----------------------------------------------------
+      // code goes here ...
+  
+      // ADD LABELS TO PANELS -----------------------------------------
+      // code goes here ...
+      
+      // and two random cards in the play region (simulating a computer/hum ply)
+      // code goes here ...
+
+      // show everything to the user
+      myCardTable.setVisible(true);
+   }
+   
+   private static class CardTable extends JFrame
+   {
+      static int MAX_CARDS_PER_HAND = 56;
+      static int MAX_PLAYERS = 2;  // for now, we only allow 2 person games    
+      private int numCardsPerHand;
+      private int numPlayers;
+      public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
+      
+      // TODO
+      public CardTable(String title, int numCardsPerHand, int numPlayers)
+      {
+         
+      }
+   }
 }
+
