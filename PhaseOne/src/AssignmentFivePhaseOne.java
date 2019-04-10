dd8c2189592ce1f3e0d24b88891c01d775eb2ae8
Andrew <ameraz@csumb.edu>
2019-04-09 22:34:05 -0700

    Updated PhaseOne to use ImageIcon instead of FIle

--- PhaseOne/src/AssignmentFivePhaseOne.java
+++ PhaseOne/src/AssignmentFivePhaseOne.java
@@ -1,24 +1,13 @@
-import java.awt.FlowLayout;
-import java.io.File;
-
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.*;
 /**
- * This application simply shows off each of the card images inside a JFrame
+ * Reads and displays the .gif image files of the cards. 
  * 
  * @author Team 6: Jared Cheney, Andrew Meraz, Chul Kim and Agustin Garcia
  *
  */
 public class AssignmentFivePhaseOne
 {
-   public static final String IMAGE_FOLDER_NAME = "images";
-
-   // static for the 57 icons and their corresponding labels
-   // normally we would not have a separate label for each card, but
-   // if we want to display all at once using labels, we need to.
-   
    static final int NUM_CARD_IMAGES = 57; // 52 + 4 jokers + 1 back-of-card image
    static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
       
@@ -28,15 +17,16 @@ public class AssignmentFivePhaseOne
       // in a SHORT loop.  For each file name, read it in and use it to
       // instantiate each of the 57 Icons in the icon[] array.
       //exclude BK.gif (back image)
-       File folder = new File(IMAGE_FOLDER_NAME);
-       File[] listOfFiles = folder.listFiles();
-       if (listOfFiles != null) {
-          for (int i = 0; i < listOfFiles.length; i++) {
-             String fileName = listOfFiles[i].getName();
-             if (listOfFiles[i].isFile() && !fileName.equals("BK.gif")) {
-                icon[i] = new ImageIcon(IMAGE_FOLDER_NAME + "/" +  fileName);
-             }
-         }
+       int x = 0;
+       for (int j = 0; j <4; j++)
+       {
+          for (int i = 0; i < 14; i++)
+          {
+             
+             icon[x] = new ImageIcon("images/" + turnIntIntoCardValue(i) + 
+                   turnIntIntoCardSuit(j) +".gif");
+             x++;
+          }  
        }
    }
    
@@ -44,82 +34,40 @@ public class AssignmentFivePhaseOne
    static String turnIntIntoCardValue(int k)
    {
       // an idea for a helper method (do it differently if you wish)
-      String ret = "";
-      switch (k) {
-      case 0:
-         ret = "A";
-         break;
-      case 1:
-         ret = "2";
-         break;
-      case 2:
-         ret = "3";
-         break;
-      case 3:
-         ret = "4";
-         break;
-      case 4:
-         ret = "5";
-         break;
-      case 5:
-         ret = "6";
-         break;
-      case 6:
-         ret = "7";
-         break;
-      case 7:
-         ret = "8";
-         break;
-      case 8:
-         ret = "9";
-         break;
-      case 9:
-         ret = "10";
-         break;
-      case 10:
-         ret = "J";
-         break;
-      case 11:
-         ret = "Q";
-         break;
-      case 12:
-         ret = "K";
-         break;
-      case 13:
-         ret = "K";
-         break;
-      default:
-         throw new IllegalArgumentException();
+      if (k == 0) {
+         return "A";
+      } else if (k == 9) {
+         return "T";
+      } else if (k == 10) {
+         return "J";
+      } else if (k == 11) {
+         return "Q";
+      } else if (k == 12) {
+         return "K";
+      } else if (k == 13) {
+         return "X";
+      } else {
+         return Integer.toString(++k);
       }
-      return ret;
    }
    
    // turns 0 - 3 into "C", "D", "H", "S"
    static String turnIntIntoCardSuit(int j)
    {
-      // an idea for another helper method (do it differently if you wish)
-      String ret = "";
-      switch (j) {
-      case 0:
-         ret = "C";
-         break;
-      case 1:
-         ret = "D";
-         break;
-      case 2:
-         ret = "H";
-         break;
-      case 3:
-         ret = "S";
-         break;
-      default:
-         throw new IllegalArgumentException();
+      if (j == 0) {
+         return "C";
+      } else if (j == 1) {
+         return "D";
+      } else if (j == 2) {
+         return "H";
+      } else {
+         return "S";
       }
-      return ret;
+         
    }
    
    // a simple main to throw all the JLabels out there for the world to see
-   // this was the main that was provided in the assignment
+   // TODO: this was the main that was provided in the assignment
    public static void main(String[] args)
    {
       int k;
